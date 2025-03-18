f9241565f8fb8dc097255ebc4d0850441253adb2
IonizedAlkaline <rudranshm007@gmail.com>
2025-03-17 19:55:05 -0700

    Add files via upload

---
+++ Age.py
@@ -0,0 +1,8 @@
+age = int(input("Enter your age: "))
+if age >= 20:
+    if age <= 30:
+        print("Your age is between 20 and 30")
+    else:
+        print("Your age is not between 20 and 30")
+else:
+    print("Your age is not between 20 and 30")
